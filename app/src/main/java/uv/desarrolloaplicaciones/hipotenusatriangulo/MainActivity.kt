4f85554a70fb4bdfc424c86475abea465d9026bc
Asis <asis.reyes@hotmail.com>
2021-10-14 16:29:42 -0500

    Código separado en más métodos para evitar The Blob.

--- app/src/main/java/uv/desarrolloaplicaciones/hipotenusatriangulo/MainActivity.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/hipotenusatriangulo/MainActivity.kt
@@ -5,37 +5,54 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.*
 import android.widget.Toast
+import kotlin.text.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnCalcular.setOnClickListener{
-            guardarCatetos();
+            guardarCatetos()
         }
         btnLimpiar.setOnClickListener{
-            limpiarCampos();
+            limpiarCampos()
         }
 
     }
 
-    fun guardarCatetos(){
-        var catetob = editTextCatetoB.text.toString();
-        var catetoc = editText_CatetoC.text.toString();
+    fun calcularHipotenusa(B: String, C: String){
         val exponente = 2
-        var bCuadrado = Math.pow(catetob.toDouble(), exponente.toDouble());
-        var cCuadrado = Math.pow(catetoc.toDouble(), exponente.toDouble());
-        val resultadoCuadrado = bCuadrado + cCuadrado;
-        val resultado = Math.sqrt(resultadoCuadrado);
-        print(resultado);
-        textViewHipotenusa.text = resultado.toString();
+        val bCuadrado =  Math.pow(B.toDouble(), exponente.toDouble())
+        val cCuadrado =  Math.pow(C.toDouble(), exponente.toDouble())
+        val resultadoCuadrado = bCuadrado + cCuadrado
+        val resultado = Math.sqrt(resultadoCuadrado)
+        textViewHipotenusa.text = resultado.toString()
         val mensaje = "Hipotenusa: ${resultado}"
         Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
     }
 
+    fun String.intOrString() = toIntOrNull() ?: this
+
+    fun guardarCatetos(){
+        editTextCatetoB.text.toString().intOrString()
+        editText_CatetoC.text.toString().intOrString()
+        if(editTextCatetoB.text.toString().isNotEmpty() && editText_CatetoC.text.toString().isNotEmpty()
+            && editTextCatetoB.text.toString() != "." && editTextCatetoB.text.toString() != ","
+            && editText_CatetoC.text.toString() != "." && editText_CatetoC.text.toString() != ","){
+            val catetob = editTextCatetoB.text.toString()
+            val catetoc = editText_CatetoC.text.toString()
+            calcularHipotenusa(catetob, catetoc)
+            }
+        else{
+            val mensaje = "Debes ingresar ambos catetos y ambos deben ser números enteros"
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
     fun limpiarCampos(){
-        editTextCatetoB.text.clear();
-        editText_CatetoC.text.clear();
-        textViewHipotenusa.text = "";
+        editTextCatetoB.text.clear()
+        editText_CatetoC.text.clear()
+        textViewHipotenusa.text = ""
     }
 }
